f6cea914751504fa864be7ac0007a85862a1a3e0
prithvivarma-nallapuraju <nvpprithvivarma@gmail.com>
2023-04-06 10:29:23 +0530

    added some little patches on styling

--- components/button/button.jsx
+++ components/button/button.jsx
@@ -1,6 +1,6 @@
 const button = (props) => {
     return (
-        <button className="bg-amber-600 mx-auto p-4 rounded-lg text-white font-Alkatra text-2xl flex justify-stretch h-16" onClick={props.onClick}> {props.value}</button>
+        <button className="bg-amber-600 my-4 sm:mx-auto p-4 rounded-lg text-white font-Alkatra text-2xl flex justify-stretch h-16" onClick={props.onClick}> {props.value}</button>
     )
 }
 
--- components/output/output.jsx
+++ components/output/output.jsx
@@ -6,10 +6,10 @@ const output = (props) => {
         <div className="flex flex-col justify-center mx-auto lg:w-3/4 m-1 p-4">
             {props.todoList.map((todo,index)=>{
                 return (
-                    <div key={index} className="flex justify-end bg-slate-300 m-4 p-4 rounded-lg">
+                    <div key={index} className="flex flex-col sm:flex-row justify-end bg-slate-300 m-4 p-4 rounded-lg">
                         <div className="flex flex-col w-3/4 text-start">
-                            <h5 className="text-3xl font-Alkatra font-extrabold text-red-600">{todo[0]}</h5>
-                            <p className="text-xl font-Alkatra font-medium">{todo[1]}</p>
+                            <h5 className="text-3xl font-Alkatra font-extrabold text-red-600 overflow-auto whitespace-pre-line">{todo[0]}</h5>
+                            <p className="text-xl font-Alkatra font-medium overflow-visible whitespace-pre-line">{todo[1]}</p>
                         </div>
                         <Button value="Delete" onClick = {(index)=> props.deleteHandler(index)}/>
                     </div> 
